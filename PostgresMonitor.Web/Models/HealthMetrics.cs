106c5fd5611142534c9b07828d1d5e1670fa8a60
N3agu <andreei1465@yahoo.com>
2026-05-24 17:10:08 +0000

    feat: add HealthMetrics model

---
+++ PostgresMonitor.Web/Models/HealthMetrics.cs
@@ -0,0 +1,13 @@
+﻿namespace PostgresMonitor.Web.Models
+{
+    public class HealthMetrics
+    {
+        public DateTime Timestamp { get; set; }
+
+        public int ActiveConnections { get; set; }
+        public int SlowQueries { get; set; }
+
+        public double CpuUsage { get; set; }
+        public double MemoryUsage { get; set; }
+    }
+}
